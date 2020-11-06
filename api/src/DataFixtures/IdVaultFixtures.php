df81f356dfbc434dc3461d55cf29f27de9bc4232
Barry Brands <barrybrands02@hotmail.com>
2020-11-06 16:18:23 +0100

    Commonground.nu application added

--- api/src/DataFixtures/IdVaultFixtures.php
+++ api/src/DataFixtures/IdVaultFixtures.php
@@ -231,5 +231,19 @@ class IdVaultFixtures extends Fixture
         $manager->persist($dossier);
         $manager->flush();
         $manager->getRepository('App:Dossier')->findOneBy(['id'=> $id]);
+
+        // Commonground
+        $id = Uuid::fromString('62817d5c-0ba5-4aaa-81f2-ad0e5a763cdd');
+        $application = new Application();
+        $application->setName('Commonground.nu');
+        $application->setDescription('Commonground.nu application');
+        $application->setAuthorizationUrl('https://dev.commongroud.nu/auth/idvault');
+        $application->setOrganization($this->commonGroundService->cleanUrl(['component' => 'wrc', 'type' => 'organizations', 'id' => '073741b3-f756-4767-aa5d-240f167ca89d'])); //Conduction
+        $application->setContact($this->commonGroundService->cleanUrl(['component' => 'wrc', 'type' => 'applications', 'id' => '7d19fbc6-6c35-4087-ab10-9778277cefe1'])); //Commonground.nu
+        $manager->persist($application);
+        $application->setId($id);
+        $manager->persist($application);
+        $manager->flush();
+        $application = $manager->getRepository('App:Application')->findOneBy(['id'=> $id]);
     }
 }
