e240e617e88a1acea78ffd88b80d74e2811b4214
ginokok1996 <ginokok1996@hotmail.com>
2021-01-06 13:34:23 +0100

    membership object

---
+++ api/src/Entity/Membership.php
@@ -0,0 +1,250 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\DateFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use App\Repository\MembershipRepository;
+use DateTime;
+use DateTimeInterface;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\MaxDepth;
+use Symfony\Component\Validator\Constraints as Assert;
+/**
+ * An entity representing a membership
+ *
+ * entity that holds the memeberships linked to groups
+ *
+ * @author Gino Kok <gino@conduction.nl>
+ *
+ * @category entity
+ *
+ * @license EUPL <https://github.com/ConductionNL/wallet-component/blob/master/LICENSE.md>
+ *
+ * @ApiResource(
+ *     normalizationContext={"groups"={"read"}, "enable_max_depth"=true},
+ *     denormalizationContext={"groups"={"write"}, "enable_max_depth"=true},
+ *     itemOperations={
+ *          "get",
+ *          "put",
+ *          "delete",
+ *          "get_change_logs"={
+ *              "path"="/memberships/{id}/change_log",
+ *              "method"="get",
+ *              "swagger_context" = {
+ *                  "summary"="Changelogs",
+ *                  "description"="Gets al the change logs for this resource"
+ *              }
+ *          },
+ *          "get_audit_trail"={
+ *              "path"="/memberships/{id}/audit_trail",
+ *              "method"="get",
+ *              "swagger_context" = {
+ *                  "summary"="Audittrail",
+ *                  "description"="Gets the audit trail for this resource"
+ *              }
+ *          }
+ *     },
+ *     collectionOperations={
+ *          "get",
+ *          "post"
+ *     }
+ * )
+ *
+ * @ORM\Entity(repositoryClass=MembershipRepository::class)
+ * @Gedmo\Loggable(logEntryClass="Conduction\CommonGroundBundle\Entity\ChangeLog")
+ * @ORM\HasLifecycleCallbacks
+ *
+ * @ApiFilter(OrderFilter::class)
+ * @ApiFilter(DateFilter::class, strategy=DateFilter::EXCLUDE_NULL)
+ * @ApiFilter(SearchFilter::class)
+ */
+class Membership
+{
+    /**
+     * @var UuidInterface The UUID identifier of this object
+     *
+     * @example e2984465-190a-4562-829e-a8cca81aa35d
+     *
+     * @Groups({"read"})
+     * @Assert\Uuid
+     * @ORM\Id
+     * @ORM\Column(type="uuid", unique=true)
+     * @ORM\GeneratedValue(strategy="CUSTOM")
+     * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
+     */
+    private $id;
+
+    /**
+     * @var string User uri from user component
+     *
+     * @example https://example.nl/user
+     *
+     * @Groups({"read","write"})
+     * @Assert\NotNull
+     * @ORM\Column(type="string", length=255, nullable=false)
+     */
+    private $userUrl;
+
+    /**
+     * @Groups({"read", "write"})
+     * @MaxDepth(1)
+     * @ORM\ManyToOne(targetEntity=Group::class, inversedBy="memberships")
+     */
+    private $userGroup;
+
+    /**
+     * @var DateTime The moment the invite was send
+     *
+     * @example 20190101
+     * @Groups({"read"})
+     * @Gedmo\Timestampable(on="create")
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $dateInvited;
+
+    /**
+     * @var DateTime The moment the invite was accepted by the group
+     *
+     * @example 20190101
+     *
+     * @Groups({"read", "write"})
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $dateAcceptedGroup;
+
+    /**
+     * @var DateTime The moment the invite was accepted by the user
+     *
+     * @example 20190101
+     *
+     * @Groups({"read", "write"})
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $dateAcceptedUser;
+
+    /**
+     * @var DateTime The moment this request was created by the submitter
+     *
+     * @example 20190101
+     *
+     * @Groups({"read"})
+     * @Gedmo\Timestampable(on="create")
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $dateCreated;
+
+    /**
+     * @var DateTime The moment this request was created by the submitter
+     *
+     * @example 20190101
+     *
+     * @Groups({"read"})
+     * @Gedmo\Timestampable(on="update")
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $dateModified;
+
+    public function getId(): UuidInterface
+    {
+        return $this->id;
+    }
+
+    public function setId(UuidInterface $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getUserUrl(): ?string
+    {
+        return $this->userUrl;
+    }
+
+    public function setUserUrl(string $userUrl): self
+    {
+        $this->userUrl = $userUrl;
+
+        return $this;
+    }
+
+    public function getUserGroup(): ?Group
+    {
+        return $this->userGroup;
+    }
+
+    public function setUserGroup(?Group $userGroup): self
+    {
+        $this->userGroup = $userGroup;
+
+        return $this;
+    }
+
+    public function getDateAcceptedUser(): ?\DateTimeInterface
+    {
+        return $this->dateAcceptedUser;
+    }
+
+    public function setDateAcceptedUser(DateTimeInterface $dateAcceptedUser): self
+    {
+        $this->dateAcceptedUser = $dateAcceptedUser;
+
+        return $this;
+    }
+
+    public function getDateAcceptedGroup(): ?\DateTimeInterface
+    {
+        return $this->dateAcceptedGroup;
+    }
+
+    public function setDateAcceptedGroup(DateTimeInterface $dateAcceptedGroup): self
+    {
+        $this->dateAcceptedGroup = $dateAcceptedGroup;
+
+        return $this;
+    }
+
+    public function getDateInvited(): ?\DateTimeInterface
+    {
+        return $this->dateInvited;
+    }
+
+    public function setDateInvited(DateTimeInterface $dateInvited): self
+    {
+        $this->dateInvited = $dateInvited;
+
+        return $this;
+    }
+
+    public function getDateCreated(): ?\DateTimeInterface
+    {
+        return $this->dateCreated;
+    }
+
+    public function setDateCreated(DateTimeInterface $dateCreated): self
+    {
+        $this->dateCreated = $dateCreated;
+
+        return $this;
+    }
+
+    public function getDateModified(): ?DateTimeInterface
+    {
+        return $this->dateModified;
+    }
+
+    public function setDateModified(DateTimeInterface $dateModified): self
+    {
+        $this->dateModified = $dateModified;
+
+        return $this;
+    }
+}
